622f7704811976b8cf10e501778a93eee9239c0f
alina <alina.chernok@gmail.com>
2024-03-17 06:32:12 +0300

    start to add chat

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,15 @@ std::string server_address("0.0.0.0:50051");
 void run_server() {
     auth_service auth_service;
     ProfileServiceImpl profile_service;
+    InvitationServiceImpl invitation_service;
+    ChatServiceImpl chat_service;
 
     grpc::ServerBuilder builder;
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&auth_service);
     builder.RegisterService(&profile_service);
+    builder.RegisterService(&invitation_service);
+    builder.RegisterService(&chat_service);
 
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << server_address << '\n';
